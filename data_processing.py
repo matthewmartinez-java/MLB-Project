862c2b20d398cd4345bb17481f61bb2b345dd942
Matthew Martinez <152852397+matthewmartinez-java@users.noreply.github.com>
2025-06-04 00:30:41 -0700

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -1,9 +1,1 @@
-import pandas as pd
 
-def load_data(file_path = 'data/player_stats.csv'):
-    data = pd.read_csv(file_path)
-    data = data.dropna()
-    return data
-
-def get_player_stats(data, player_name):
-    return data[data['player'] == player_name]
